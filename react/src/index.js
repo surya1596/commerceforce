e7cb05f6902a4c04600c9399995217f3ee43c044
surya1596 <44322371+surya1596@users.noreply.github.com>
2022-05-23 23:33:55 +0530

    Add files via upload

---
+++ react/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Calculator from './components/calculator.js';
+  
+// Render the Calculator to the Web page.
+ReactDOM.render(<Calculator />, document.getElementById('root'));
